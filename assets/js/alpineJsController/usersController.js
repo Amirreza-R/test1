2bdbbc2e731568046548d122ccc968f32facd620
Mr.Razavi <amirreza.razavi7325@gmail.com>
2024-10-23 19:46:19 +0330

    part 26

--- assets/js/alpineJsController/usersController.js
+++ assets/js/alpineJsController/usersController.js
@@ -2,14 +2,19 @@ document.addEventListener("alpine:init", () => {
     Alpine.data("usersData", function () {
         return {
             users: [],
+            pageUsers: [],
             isLoading: false,
             showAddModal: false,
+            pageCount: 1,
+            itemsCount: 4,
+            currentPage: 1,
             getUsers() {
                 (this.isLoading = true),
                     axios
                         .get("https://jsonplaceholder.typicode.com/users")
                         .then((res) => {
                             this.users = res.data;
+                            this.pagination();
                         })
                         .catch((error) => {
                             console.log(error.message);
@@ -18,6 +23,28 @@ document.addEventListener("alpine:init", () => {
                             this.isLoading = false;
                         });
             },
+            pagination() {
+                this.pageCount = Math.ceil(this.users.length / this.itemsCount); // 10 / 4 = 3
+                let start = (this.currentPage * this.itemsCount) - this.itemsCount;
+                let end = this.currentPage * this.itemsCount;
+                this.pageUsers = this.users.slice(start, end);
+            },
+            nextPage(){
+                this.currentPage ++;
+                if(this.currentPage > this.pageCount) this.currentPage = this.pageCount
+                this.pagination();
+            },
+            previousPage(){
+                this.currentPage --;
+                if(this.currentPage < 1) this.currentPage = 1
+                this.pagination();
+            },
+            handleChangeItemsCount(value){
+                this.currentPage = 1
+                if(value < 1) this.itemsCount = 1
+                if(value > this.users.length) this.itemsCount = this.users.length
+                this.pagination()
+            }
         };
     });
 });
