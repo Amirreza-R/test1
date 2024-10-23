0cfdab23e367a481fe1f773e4d2fc09d375b6b1d
Mr.Razavi <amirreza.razavi7325@gmail.com>
2024-10-23 11:51:20 +0330

    part12

--- assets/js/users.js
+++ assets/js/users.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     var elems = document.querySelectorAll(".collapsible");
     var instances = M.Collapsible.init(elems, {});
 });
+
+const addBtn = document.getElementById("add-user-btn");
+
+addBtn.addEventListener("click", ()=>{
+    document.querySelector(".add-user-modal-back").classList.remove("dis-none");
+    document.querySelector(".add-user-modal").classList.add("show");
+})
+
+document.querySelector(".add-user-modal-back").addEventListener("click",(e)=>{
+    document.querySelector(".add-user-modal").classList.remove("show");
+    e.target.classList.add("dis-none")
+})
